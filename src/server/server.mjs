7958e61e1f137e4a4b0f375c3fa0e6c701f63024
marta <marta.vila.garrido@gmail.com>
2023-06-08 19:11:50 -0700

    [feature] added server error handling

--- src/server/server.mjs
+++ src/server/server.mjs
@@ -17,30 +17,35 @@ dotenv.config({path: pathToEnv});
 const API_KEY = process.env.API_KEY;
 
 app.post('/fetchData', async (req, res) => {
-    const bookName = req.body.bookName;
-    const numSongs = req.body.numSongs;
-    const response = await fetch("https://api.openai.com/v1/chat/completions", {
-        method: "POST",
-        headers: {
-            Authorization: `Bearer ${API_KEY}`,
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            model: "gpt-3.5-turbo",
-            messages: [{
-                role: "system",
-                content: "You are a helpful assistant."
-            }, {
-                role: "user",
-                content: `Suggest a playlist of ${numSongs} songs for the book ${bookName}. Show the list of songs only.`
-            }],
-            max_tokens: 500,
-            temperature: 0.5
-        })
-    })
-    const data = await response.json()
-    res.json(data);
-})
+    try {
+        const bookName = req.body.bookName;
+        const numSongs = req.body.numSongs;
+        const response = await fetch("https://api.openai.com/v1/chat/completions", {
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${API_KEY}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                model: "gpt-3.5-turbo",
+                messages: [{
+                    role: "system",
+                    content: "You are a helpful assistant."
+                }, {
+                    role: "user",
+                    content: `Suggest a playlist of ${numSongs} songs for the book ${bookName}. Show the list of songs only.`
+                }],
+                max_tokens: 500,
+                temperature: 0.5
+            })
+        });
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        console.error('Server error:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 app.listen(3000, () => {
     console.log('Server running on port 3000');
