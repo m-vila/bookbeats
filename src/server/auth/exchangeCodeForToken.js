9ba380ee3890d558826dabb5262eaede1de39373
marta <marta.vila.garrido@gmail.com>
2023-06-12 16:57:44 -0700

    [chore] created auth folder and moved token access files

---
+++ src/server/auth/exchangeCodeForToken.js
@@ -0,0 +1,32 @@
+import fetch from 'node-fetch';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const clientId = process.env.SPOTIFY_CLIENT_ID;
+const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+const redirectUri = 'http://localhost:3000/callback';
+
+export const exchangeCodeForToken = async (authorizationCode) => {
+    try {
+        const response = await fetch('https://accounts.spotify.com/api/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: new URLSearchParams({
+                grant_type: 'authorization_code',
+                code: authorizationCode,
+                redirect_uri: redirectUri,
+                client_id: clientId,
+                client_secret: clientSecret
+            }),
+        });
+
+        const data = await response.json();
+        return { accessToken: data.access_token, refreshToken: data.refresh_token };
+    } catch (error) {
+        console.error('Error exchanging code for token:', error);
+        return null;
+    }
+};
