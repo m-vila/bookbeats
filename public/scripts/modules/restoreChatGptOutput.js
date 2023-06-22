f2e8b5655ef8035f589a347468b72bbb8c04feb5
marta <marta.vila.garrido@gmail.com>
2023-06-21 22:51:58 -0700

    [refactor] added useful comments

--- public/scripts/modules/restoreChatGptOutput.js
+++ public/scripts/modules/restoreChatGptOutput.js
@@ -1,11 +1,14 @@
+// Function to restore chat GPT output using local storage
 export const restoreChatGptOutput = () => {
     const chatGptOutput = localStorage.getItem('chatGptOutput');
+    // Retrieve chat GPT output from local storage
     if (chatGptOutput) {
         const chatGptOutputElement = document.getElementById('chatGptOutput');
         chatGptOutputElement.innerHTML = chatGptOutput;
         chatGptOutputElement.style.display = 'block';
         localStorage.removeItem('chatGptOutput');
 
+        //Display the "Open playlist with Spotify" button if the chat GPT output is not empty
         const openPlaylistWithSpotifyButton = document.getElementById('openPlaylistWithSpotify');
         if (chatGptOutput.trim().length > 0) {
             openPlaylistWithSpotifyButton.style.display = 'block';
